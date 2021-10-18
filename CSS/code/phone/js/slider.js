4e4c9b13d217e5b57846032d864eb57a8bbf1455
Huy Nguyen <nguyenquochuyhbt@gmail.com>
2021-10-18 08:25:35 +0700

    commit

---
+++ CSS/code/phone/js/slider.js
@@ -0,0 +1,21 @@
+/* Glide JS Carousel */
+const slider1 = document.getElementById("glide_1");
+
+
+/* Hero */
+if (slider1) {
+    new Glide(slider1, {
+        type: "carousel",
+        startAt: 0,
+        autoplay: 3000,
+        hoverpause: true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFuc: "linear",
+    }).mount();
+}
+
+/* Latest Products */
+
+
+/* Testimonial */
